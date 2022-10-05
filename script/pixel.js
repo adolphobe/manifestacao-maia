33e22a38d55f62a39c3e8d5875d6832c6aaf093c
adolphobe <56713136+adolphobe@users.noreply.github.com>
2022-10-05 13:12:08 -0300

    ,

--- script/pixel.js
+++ script/pixel.js
@@ -1,3 +1,4 @@
+//FACEBOOK
 !function(f,b,e,v,n,t,s)
 {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
 n.callMethod.apply(n,arguments):n.queue.push(arguments)};
@@ -7,3 +8,18 @@ t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
 fbq('init', '543185694227266');
+
+
+
+//TABOOLA
+var taboola_pixel = 1493255;
+
+window._tfa = window._tfa || [];
+!function (t, f, a, x) {
+       if (!document.getElementById(x)) {
+          t.async = 1;t.src = a;t.id=x;f.parentNode.insertBefore(t, f);
+       }
+}(document.createElement('script'),
+document.getElementsByTagName('script')[0],
+'//cdn.taboola.com/libtrc/unip/'+taboola_pixel+'/tfa.js',
+'tb_tfa_script');
